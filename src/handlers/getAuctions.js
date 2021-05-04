a418ffdf9abcc7955f79bd8bf96dc24a0651a35b
Melissa Pereira <melissa.pereira@kinandcarta.com>
2021-05-04 14:06:14 +0100

    Created a common middleware

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,9 +1,6 @@
 import AWS from 'aws-sdk';
 import process from "process";
-import middy from '@middy/core';
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
+import commonMiddleware from "../lib/commonMiddleware";
 import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
@@ -28,6 +25,4 @@ async function getAuctions(event, context) {
     };
 }
 
-export const handler = middy(getAuctions).use(httpJsonBodyParser()).use(httpEventNormalizer()).use(httpErrorHandler());
-
-
+export const handler = commonMiddleware(getAuctions);
